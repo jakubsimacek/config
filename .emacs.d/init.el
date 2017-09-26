966520c46ac3964bd523bd998da5fc903fe83072
Jakub Simacek <jakubsimacek@email.cz>
2017-09-26 10:29:34 +0200

    merging emacs configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,11 @@
   (package-install 'use-package))
 (require 'use-package)
 
+;;(require 'pug-mode)
+(global-linum-mode t)
+(setq inhibit-startup-screen t)
+(setq-default frame-title-format '("%b %f [%m]"))
+
 (use-package scala-mode
   :interpreter
   ("scala" . scala-mode))
@@ -64,16 +69,17 @@
      ;; rebind "C-h ..." to "M-m ..." to preserve functionality
      (define-key helm-find-files-map (kbd "M-m") helm-find-files-C-h-map)))
 
-(global-set-key (kbd "C-h") 'backward-char)
+(global-set-key (kbd "M-h") help-map)
+(global-set-key (kbd "C-h") 'left-char)
 (global-set-key (kbd "C-j") 'next-line)
 (global-set-key (kbd "C-k") 'previous-line)
-(global-set-key (kbd "C-l") 'forward-char)
-
-(menu-bar-mode 0)
-(fset `yes-or-no-p `y-or-n-p)
+(global-set-key (kbd "C-l") 'right-char)
+(global-set-key (kbd "C-i") 'kill-line)
 
 (global-set-key (kbd "M-]") 'next-buffer)
 (global-set-key (kbd "M-[") 'previous-buffer)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
 
 ;; nodejs REPL prompt fix
 (setenv "NODE_NO_READLINE" "1")
@@ -90,3 +96,41 @@
 (add-hook 'java-mode-hook 'eclim-mode)
 
 ;;(require 'eclimd)
+
+(require 'genrnc)
+(setq genrnc-user-schemas-directory "~/.emacs.d/schema")
+
+
+(when (eq system-type 'ms-windows)
+  (setq shell-file-name "bash")
+  (w32-send-sys-command #xf030)
+)
+
+(add-hook 'window-setup-hook (lambda () (tool-bar-mode -1)))
+
+(menu-bar-mode 0)
+(fset `yes-or-no-p `y-or-n-p)
+
+;;(use-package ensime
+;;  :ensure t
+;;  :pin melpa-stable)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (eclim genrnc use-package helm ensime pug-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'genrnc)
+(setq genrnc-user-schemas-directory "~/.emacs.d/schema")
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
